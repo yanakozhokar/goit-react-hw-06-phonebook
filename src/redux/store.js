2e68264ce15025fd9a484bc0c00b302cb3b740a8
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-03-04 11:47:45 +0200

    localstorage updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsSlice } from './contactsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // whitelist: ['contactsList'],
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsSlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
+    contacts: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
