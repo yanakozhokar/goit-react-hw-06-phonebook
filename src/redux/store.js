56a65850eb480dbc95b05f5fa95a2f9830ef4bc9
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-03-04 11:59:08 +0200

    fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { contactsSlice } from './contactsSlice';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 const persistConfig = {
   key: 'root',
   storage,
-  // whitelist: ['contactsList'],
+  whitelist: ['contactsList'],
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsSlice.reducer);
@@ -15,6 +23,12 @@ export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persistor = persistStore(store);
+export let persistor = persistStore(store);
